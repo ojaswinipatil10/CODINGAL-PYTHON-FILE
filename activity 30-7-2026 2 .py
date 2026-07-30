f4fe7d4a700776715b4893048e8f4a8fec21abb0
ojaswinipatil10 <Ojaswinipatil10@gmail.com>
2026-07-30 16:55:13 +0400

    commit

--- activity 30-7-2026 2 .py
+++ activity 30-7-2026 2 .py
@@ -2,10 +2,13 @@ class student:
     grade = 10
     name = "Flower"
     def introduction(self):
+        print("================")
         print("I am a student ")
+      
     def details(self):
         print("My name is " , self.name)
         print(" I study in Grade " , self.grade)
+        print("================")
 ob = student()
 ob.introduction()
 ob.details()
