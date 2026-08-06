ea73eebffa4a2c9854e48f85f0196c8a129e17fb
ojaswinipatil10 <Ojaswinipatil10@gmail.com>
2026-08-06 10:38:16 +0400

    commit

---
+++ Activity 6-8-2026.py
@@ -0,0 +1,53 @@
+class Cricket:
+    def __init__(self , player, score):
+        self.__player = player
+        self.__score = score
+    def info(self):
+        print(f"Cricket - Player :- {self.__player} , Score:- {self.__score}")
+    def play(self):
+        print(f"{self.__player} hits a six !")
+    def get_score(self):
+        return self.__score
+    def set_score(self , new_score):
+        if new_score >= 0:
+            self.__score = new_score
+            print(f"Score updated to {self.__score}")
+        else:
+            print("Score cannot be negative !.")
+            print("----------")
+
+class Football:
+    def __init__(self , player, score):
+        self.__player = player
+        self.__score = score
+
+    def info(self):
+        print(f"Football - Player:- {self.__player} , Score :- {self.__score}")
+
+    def play(self):
+        print(f"{self.__player} Scores a goal !")
+    def get_score(self):
+        return self.__score
+    def set_score(self , new_score):
+        if new_score >= 0:
+            self.__score = new_score
+            print(f"Score has been updated to {self.__score}")
+        else:
+            print("Score cannot be negative !.")
+            print("----------")
+
+cricket = Cricket("Rohit" , 85)
+football = Football ("Arjun" , 2)
+print("---SPORTS SCOREBOARD ---\n")
+for sport in (cricket , football):
+    sport.info()
+    sport.play()
+    print()
+
+print("---DIRECT CHANGE ATTEMPT ---")
+cricket.__score = 999
+print(f"get score() still shows :- {cricket.get_score()}")
+
+print("/n---Updating scores ---")
+cricket.set_score(100)
+football.set_score(3)
